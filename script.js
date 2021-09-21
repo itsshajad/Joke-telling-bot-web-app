2388bfc5698ee16a2fe63b5900c57a560656b1de
itsshajad <webcode87@gmail.com>
2021-09-21 23:41:01 +0530

    joke telling web app created

--- script.js
+++ script.js
@@ -3,54 +3,80 @@
 let jokeTextPlaceholder = document.getElementById('jokeText');
 const jokeBtn = document.getElementsByTagName('button');
 const voice = document.getElementById('voice');
+const speech = new SpeechSynthesisUtterance('No warning should arise');
 
-jokeBtn[0].addEventListener('click', function () {
+window.onload = function () {
+  jokeBtn[0].addEventListener('click', JokeApiFunction);
+};
+
+const JokeApiFunction = () => {
   fetch('https://v2.jokeapi.dev/joke/Programming,Pun?type=single')
     .then((res) => res.json())
     .then((data) => responsFunction(data));
-});
+};
 
-// const speechRecognition =
-//   window.speechRecognition || window.webkitSpeechRecognition;
+const speechRecognition =
+  window.speechRecognition || window.webkitSpeechRecognition;
 
-// const recognition = new speechRecognition();
+const recognition = new speechRecognition();
 
-// recognition.start();
+speechRecognition.lang = 'hi-IN';
 
-// console.log(recognition);
+recognition.continuous = true;
 
-// recognition.onstart = function () {
-//   console.log('recognition start');
-// };
+recognition.start();
 
-// recognition.onresult = function (event) {
-//   const transcript = event.results[0][0].transcript;
-//   const voice = synth.getVoices(transcript);
+recognition.onresult = function (event) {
+  const transcript = event.results[event.results.length - 1][0].transcript;
+  console.log(transcript);
+  if (transcript.includes('tell me joke') || transcript.includes('joke')) {
+    console.log(true);
+    jokeBtn[0].click();
+  }
+  // const voice = synth.getVoices(transcript);
 
-//   console.log(voice);
-// };
+  // console.log(voice);
+};
+
+document.onkeypress = logkey;
+function logkey(e) {
+  console.log(e);
+  if (e.code == 'KeyJ' || e.code == 'Keyj') {
+    jokeBtn[0].click();
+  }
+}
 
 const responsFunction = (data) => {
   jokeTextPlaceholder.innerHTML = data.joke;
-  // console.log(data);
   speakJoke(data.joke);
 };
-let voices = speechSynthesis.getVoices();
 
-voices.map((v, i) => {
-  const option = document.createElement('option');
-  option.value = i;
-  option.textContent = v.name;
-  voice.appendChild(option);
-});
+function updateVoice() {
+  const voices = speechSynthesis.getVoices();
+  voices.map((v, i) => {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = v.name;
+    voice.appendChild(option);
+  });
 
-console.log(voices);
+  voice.addEventListener('change', (e) => {
+    speechSynthesis.cancel();
+    const selectedVoice = e.target.value;
+    speech.voice = voices[selectedVoice];
+  });
+}
 
 const speakJoke = (message) => {
-  const speech = new SpeechSynthesisUtterance();
   speech.text = message;
   speech.volume = 1;
   speech.pitch = 1;
   speech.rate = 1;
-  window.speechSynthesis.speak(speech);
+  speechSynthesis.speak(speech);
 };
+
+speechSynthesis.addEventListener('voiceschanged', updateVoice);
+
+// setTimeout(() => {
+//   alert('press j in you keyboard to listen joke');
+// }, 3000);
